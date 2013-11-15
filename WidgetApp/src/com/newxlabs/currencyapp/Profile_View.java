4abc1749f29b2e0158e1592228d1c3ec280837d0
dlilaramani <dlilaramani@infinite-ind.com>
2013-11-15 19:54:06 +0530

    config file added with currencies

--- WidgetApp/src/com/newxlabs/currencyapp/Profile_View.java
+++ WidgetApp/src/com/newxlabs/currencyapp/Profile_View.java
@@ -1,7 +1,12 @@
 package com.newxlabs.currencyapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -26,6 +31,25 @@ public class Profile_View extends Activity {
 
 		updateBtn = (Button) findViewById(R.id.updateBtn);
 		
+		
+		// Spinner click listener
+		//fromSpinner.setOnItemSelectedListener((OnItemSelectedListener) this);
+ 
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("USD");
+        categories.add("INR");
+        categories.add("EUR");
+        categories.add("GBP");
+        categories.add("AUD");
+        categories.add("CAD");
+ 
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+ 
+        // attaching data adapter to spinner
+        fromSpinner.setAdapter(dataAdapter);
+        toSpinner.setAdapter(dataAdapter);
 	}
 
 }
